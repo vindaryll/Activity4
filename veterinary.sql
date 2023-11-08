a8cccad5606e7afc0a2bf114950dad3966dc550f
vindaryll <melvindaryll01@gmail.com>
2023-11-08 11:51:36 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,12 @@
 -- feat/create-database-veterinary
 CREATE DATABASE veterinary;
+
+-- feat/create-table-owners
+CREATE TABLE owners(
+	ownerid INT PRIMARY KEY,
+	ofirstname VARCHAR(50) NOT NULL,
+	olastname VARCHAR(50) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	phone VARCHAR(50) NOT NULL
+);

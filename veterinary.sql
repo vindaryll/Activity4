4bd2af8b8327a6cf1a8a224a1de1e7af3f05948d
Vonrazel <bonyrazelmorales@gmail.com>
2023-11-08 11:54:35 -0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,16 @@ CREATE TABLE owners(
 	email VARCHAR(100) NOT NULL,
 	phone VARCHAR(50) NOT NULL
 );
+
+-- feat/create-table-animals
+CREATE TABLE animals(
+	animalid INT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	species VARCHAR(50) NOT NULL,
+	breed VARCHAR(50) NOT NULL,
+	dateofbirth DATE,
+	gender VARCHAR(10) NOT NULL,
+	color VARCHAR(50) NOT NULL,
+	ownerid INT,
+	foreign key (ownerid) references owners(ownerid)
+);
